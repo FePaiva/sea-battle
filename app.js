2462868a0cb7bc510a04afbc911e6d637388426e
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2023-02-02 16:38:24 -0800

    player and computer boards created.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const flipButton = document.getElementById('flip-button');
 const optionContainer = document.querySelector('.option-container');
+const gameBoardContainer = document.getElementById('game-board-container');
 
 let angle = 0;
 
@@ -18,5 +19,27 @@ function flip() {
     (optionShip) => (optionShip.style.transform = `rotate(${angle}deg)`)
   );
 }
+
+const width = 10;
+
+function createBoard(color, user) {
+  const gameBoard = document.createElement('div');
+  gameBoard.classList.add('game-board');
+  gameBoard.style.backgroundColor = color;
+  gameBoard.id = user;
+
+  for (let i = 0; i < width * width; i++) {
+    const block = document.createElement('div');
+    block.classList.add('block');
+    block.id = i;
+
+    gameBoard.append(block);
+  }
+
+  gameBoardContainer.append(gameBoard);
+}
+
+createBoard('pink', 'player');
+createBoard('yellow', 'computer');
 
 flipButton.addEventListener('click', flip);
